b2f815154901cb94f08bf67029fc9f8fecfe2e51
nicollinares <linaresgomeznicol@gmail.com>
2025-08-05 15:05:30 -0500

    Animación plataformas

--- 2_poster_interactivo/app.js
+++ 2_poster_interactivo/app.js
@@ -1,15 +1,27 @@
 const monedas = document.querySelectorAll(".coin")
-const corazon = document.querySelector(".corazon")
+const corazones = document.querySelectorAll(".corazon")
 const contadorMonedas = document.querySelector("#contador-monedas")
 const contadorCorazones = document.querySelector("#contador-corazones")
+let totalMonedas = 0
+let totalCorazones = 0
 
-console.log(monedas,corazon,contadorMonedas,contadorCorazones)
+
+/* console.log(monedas,corazon,contadorMonedas,contadorCorazones) */
 
 monedas.forEach(function(moneda){
 
     moneda.addEventListener('click', function(){
     moneda.classList.add('saltar')
+    totalMonedas++
+    contadorMonedas.textContent = totalMonedas
 }) 
-
+ 
 })
 
+corazones.forEach(function(corazon){
+    corazon.addEventListener('click', function(){
+    corazon.classList.add('saltar')
+    totalCorazones++
+    contadorCorazones.textContent = totalCorazones
+ }) 
+})
