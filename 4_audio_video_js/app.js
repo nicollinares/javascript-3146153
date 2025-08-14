693561b6d989d167ba7690b887abcf7bddfd782f
nicollinares <linaresgomeznicol@gmail.com>
2025-08-14 15:32:29 -0500

    Audio y video - parte1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,11 @@
+const video = document.querySelector('#video');
+const playVideo = document.querySelector('#play-video');
+const stopVideo = document.querySelector('#stop-video');
+const playStopImg1 = document.querySelector('#play-stop-img1');
+const playStopImg2 = document.querySelector('#play-stop-img2');
+
+console.log(video);
+console.log(playVideo);
+console.log(stopVideo);
+console.log(playStopImg1);
+console.log(playStopImg2);
